e7de7390f5b469d1f39e934b698e326fdd0aa1e6
Harshpv <harshavardhanpv2000@gmail.com>
2024-09-09 11:31:42 +0530

    initial commit

---
+++ src/main/java/com/example/bms/BmSv2Application.java
@@ -0,0 +1,13 @@
+package com.example.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmSv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmSv2Application.class, args);
+	}
+
+}
